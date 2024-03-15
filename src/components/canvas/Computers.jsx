6710d1107d57da64412913c5e9aa9d1d730b6fda
Jesse Ogola <informjesse3@gmail.com>
2024-03-15 13:01:54 +0300

    new

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -49,21 +49,21 @@ return () => {
   
   return (
     <Canvas
-    
       frameloop="demand"
       shadows
       camera={{ position: [60, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
-      <Suspense fallback={<CanvasLoader/>}>
+      <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
+          autoRotate
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers isMobile={isMobile}/>
+        <Computers isMobile={isMobile} />
       </Suspense>
-      <Preload all/>
+      <Preload all />
     </Canvas>
   );
 }
