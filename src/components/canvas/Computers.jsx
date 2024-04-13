5ca0df23cba7b043e3b10e82e5fb270b758fbef1
Jesse Ogola <informjesse3@gmail.com>
2024-04-13 12:41:54 +0300

    new

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -57,7 +57,7 @@ return () => {
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           autoRotate
-          enableZoom={false}
+          enableZoom={true}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
