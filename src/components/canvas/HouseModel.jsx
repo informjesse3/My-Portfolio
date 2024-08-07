ee836f7d1f2d65a3c6349e33b2f9f01b7a88af02
NafHomes <informjesse3@gmail.com>
2024-08-07 22:34:24 +0300

    removed zoom

--- src/components/canvas/HouseModel.jsx
+++ src/components/canvas/HouseModel.jsx
@@ -68,7 +68,7 @@ const HouseModel = () => {
         <Suspense fallback={<CanvasLoader />}>
           <OrbitControls
             autoRotate={active}
-            enableZoom={true}
+            enableZoom={false}
             maxPolarAngle={Math.PI / 2}
             minPolarAngle={Math.PI / 2}
           />
